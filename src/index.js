2e73a0026712d721d4dcb9c8dca8651ba0723019
Egba Nnaemeka <emekason128@gmail.com>
2023-01-20 12:16:13 +0100

    basic setup

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return <h1>movie_list_application</h1>
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// console.log(movie_list_application)
