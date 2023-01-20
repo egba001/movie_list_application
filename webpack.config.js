8542bd0ef6ec96a8e72f1fc756fd7196d3d2aa99
Egba Nnaemeka <emekason128@gmail.com>
2023-01-20 13:18:58 +0100

    set data

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const HtmlWebPackPlugin = require('html-webpack-plugin')
 
 const htmlPlugin = new HtmlWebpackPlugin({
     template: './src/index.html',
