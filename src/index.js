361113d4d70904daf0270fdd2c9e3f7550b74b7d
Egba Nnaemeka <emekason128@gmail.com>
2023-01-20 16:29:47 +0100

    html webpack plugin installation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("movie_list_application")
+import React from 'react';
+import RaectDOM from 'react-dom';
+
+const App = () => {
+    return <h1>movieList</h1>;
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
