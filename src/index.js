7e8789f96ca82d7dd17c4d0f443b543e1a6e7c6b
Egba Nnaemeka <emekason128@gmail.com>
2023-01-23 16:02:57 +0100

    start from beginning

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import RaectDOM from 'react-dom';
-import List from './containers/List';
-
-const App = () => {
-    return <List />;
-}
-
-ReactDOM.render(<App />, document.getElementById('root'));
