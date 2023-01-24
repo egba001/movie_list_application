c7033c7029a6909bd2de2541a293507eec9275f0
Egba Nnaemeka <emekason128@gmail.com>
2023-01-24 15:32:00 +0100

    components created

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('Rick and Morty');
+import ReactDOM from 'react-dom/client';
+import List from './components/List';
+
+function App() {
+    return (
+        <div>
+            <h1>Rick and Morty</h1>
+            <List />
+        </div>
+    );
+};
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(<App />)
