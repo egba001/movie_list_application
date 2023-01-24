3c1b11f0a98719e91272909be75e6c2bcd5d723f
Egba Nnaemeka <emekason128@gmail.com>
2023-01-24 16:10:33 +0100

    fetched api

--- src/components/List.js
+++ src/components/List.js
@@ -1,11 +1,44 @@
+import { useEffect, useState } from 'react';
 import Character from './Character';
 
 function List() {
+    /* Set the loading state to true */
+    const [loading, setLoading] = useState(true);
+    /* Prepare an array to accept data from the API */
+    const [characters, setCharacters] = useState([]);
+
+    useEffect(() => {
+        /* Fetch data from the rick and morty REST api using an await fetch function */
+        async function fetchData() {
+            const data = await fetch(
+                'https://rickandmortyapi.com/api/character');
+        /* Convert the data from the API into a JSON file and 
+        store in the result object */
+            const { results } = await data.json();
+        /* Use the setCharacter hook to change the state of the characters variable */
+            setCharacters(results);
+        /* Set loading state to false after the function has finished fetching the api */
+            setLoading(false);
+        }
+
+        fetchData();
+    }, [characters.length]);
+
     return (
         <div>
             <h2>Characters</h2>
-            <Character />
-            <Character />
+            {/* Return the character component if the fetch unctionn has finished fetching
+            data from the api else display loading state to the user interface */}
+            {loading ? (<div>Loading...</div>) : (
+                characters.map((character) => (
+                    <Character 
+                        key={character.id}
+                        name={character.name}
+                        origin={character.origin}
+                        image={character.image}
+                        />
+                ))
+            )}
         </div>
     )
 }
